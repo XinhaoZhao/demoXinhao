a7e451cfa52db7ec073bd28bc2f0e9b8f44de8b9
XinhaoZhao <isaaczhao114@gmail.com>
2022-04-29 21:07:03 -0700

    2ndcommit

--- laundryTester.java
+++ laundryTester.java
@@ -2,15 +2,16 @@ import static org.junit.Assert.*;
 import org.junit.*;
 public class laundryTester {
     public laundry l;
-    public laundry l2;
 
     @Before
     public void setUp(){
         l = new laundry(500);
     }
+    
+    public void none(){}
 
     @Test
     public void test1(){
-        assertEquals(460, l.getBalance(10));
+        assertEquals(470, l.getBalance(10));
     }
 }
